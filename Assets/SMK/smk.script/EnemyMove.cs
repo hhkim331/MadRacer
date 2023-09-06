ba09333285c2941a7fd747d9690337e785995201
alta521 <alta521521@gmail.com>
2023-09-06 20:11:58 +0900

    .
    .

--- Assets/SMK/smk.script/EnemyMove.cs
+++ Assets/SMK/smk.script/EnemyMove.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    
+    //움직이는곳.
+    float speed = 1;
+    float maxspeed = 10f;
+    public Waypoint waypoint;//향하는 곳.
+    Waypoint nextPoint;//다음 향할 곳
     void Start()
     {
         
@@ -12,6 +16,7 @@ public class EnemyMove : MonoBehaviour
 
     void Update()
     {
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, waypoint.transform.position, speed);
        
     }
 }
--- Assets/SMK/smk.script/WaypointFollow.cs
+++ Assets/SMK/smk.script/WaypointFollow.cs
@@ -9,7 +9,7 @@ public class WaypointFollow : MonoBehaviour
     //만약, waypoint를 지나면, 오브젝트의 앞방향에 위치한 다음 waypoint를 방향삼기.
     //nextpoint를 인식해서 해당 위치로 이동하기. 
 
-    Transform enemypoint;//enemypoint는 현재 자신
+    Transform point;//enemypoint는 현재 자신
     public Waypoint waypoint;//향하는 곳.
     Waypoint nextPoint;//다음 향할 곳
     void Start()
@@ -19,21 +19,23 @@ public class WaypointFollow : MonoBehaviour
             nextPoint = waypoint.nextPoint;
         }
     }
+    //ground 판정
 
     void Update()
     {
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, waypoint.transform.position, 0.1f); //이동
+        print("3");
         //(waypoint와 enemypoint 간의 거리)의 제곱이 waypoint의 반지름의 제곱보다 작거나 같을 때 (= waypoint랑 enemypoint간의 거리가 짧을때)
-        if ((waypoint.transform.position - enemypoint.transform.position).sqrMagnitude <= waypoint.radius * waypoint.radius)
+        if ((waypoint.transform.position - point.transform.position).sqrMagnitude <= waypoint.radius * waypoint.radius)
         {
             print("2");
-            nextPoint = waypoint.nextPoint;
-            if(waypoint != null)
+            waypoint = waypoint.nextPoint;
+            if (waypoint != null)
             {
                 nextPoint = waypoint.nextPoint;
             }
         }
         print("1");
         //waypoint로 이동
-        transform.position = Vector3.MoveTowards(gameObject.transform.position, waypoint.transform.position, 0.1f);
     }
 }
