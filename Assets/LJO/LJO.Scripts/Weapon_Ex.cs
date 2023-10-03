8c03088f7594173161f7d87f6e81290baf3e6822
zioleee <102352219+zioleee@users.noreply.github.com>
2023-10-03 16:05:20 +0900

    Revert "Merge branch 'zio1'"
    This reverts commit e95a7107356b4187f23a0fef8991365a1e1126e5, reversing
    changes made to 9cebc03db8f5d065e168b3dd919011671697f0b6.

--- Assets/LJO/LJO.Scripts/Weapon_Ex.cs
+++ Assets/LJO/LJO.Scripts/Weapon_Ex.cs
@@ -1,26 +1,22 @@
-//using UnityEngine;
-//using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
-//public class Weapon_Ex : MonoBehaviour
-//{
-//    public GameObject descriptionUIPrefab; // 설명 UI 프리팹
-//    private GameObject spawnedUI; // 생성된 UI 인스턴스
-//    public Transform controllerTransform; // 컨트롤러의 Transform
-//    public float rayLength = 100f; // 레이의 길이
+public class Weapon_Ex : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject descriptionUIPrefab; // 설명 UI 프리팹
+    private GameObject spawnedUI; // 생성된 UI 인스턴스
 
-//    private bool isUIActive = false; // UI 활성화 상태
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        spawnedUI = Instantiate(descriptionUIPrefab, transform.position, Quaternion.identity);
+        spawnedUI.transform.SetParent(transform, true);
+    }
 
-//    public void OnPointerEnter(PointerEventData eventData)
-//    {
-//        spawnedUI = Instantiate(descriptionUIPrefab, transform.position, Quaternion.identity);
-//        spawnedUI.transform.SetParent(transform, true);
-//    }
-
-//    public void OnPointerExit(PointerEventData eventData)
-//    {
-//        if (spawnedUI)
-//        {
-//            Destroy(spawnedUI);
-//        }
-//    }
-//}
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (spawnedUI)
+        {
+            Destroy(spawnedUI);
+        }
+    }
+}
